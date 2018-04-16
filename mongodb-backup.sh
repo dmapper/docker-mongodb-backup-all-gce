a59c74388781b597c2c9c9254ffb89aa7fbddbff
zheoreh <oeswww@gmail.com>
2018-04-16 18:17:43 +0300

    implemented list of databases

--- mongodb-backup.sh
+++ mongodb-backup.sh
@@ -2,29 +2,37 @@
 
 # Settings
 DB_HOST="$MONGO_HOST"
+DB_NAMES="$MONGO_DATABASES"
 DB_USER="$MONGO_USER"
 DB_PASS="$MONGO_PASS"
+
 BUCKET_NAME="$BUCKET"
-FOLDER_NAME="$FOLDER"
 
 # Path in which to create the backup (will get cleaned later)
 BACKUP_PATH="/mnt/data/dump/"
 
 CURRENT_DATE=$(date +"%Y%m%d-%H%M")
 
+for DB_NAME in $DB_NAMES
+do
+
 # Backup filename
-BACKUP_FILENAME="$CURRENT_DATE.tar.gz"
+BACKUP_FILENAME="$DB_NAME-$CURRENT_DATE.tar.gz"
 
 # Create the backup
-mongodump -h "$DB_HOST" -u "$DB_USER" -p "$DB_PASS" -o "$BACKUP_PATH"
+mongodump -h "$DB_HOST" -d "$DB_NAME" -u "$DB_USER" -p "$DB_PASS" -o "$BACKUP_PATH"
 cd $BACKUP_PATH || exit
 
 # Archive and compress
 tar -cvzf "$BACKUP_PATH""$BACKUP_FILENAME" ./*
 
 # Copy to Google Cloud Storage
-echo "Copying $BACKUP_PATH$BACKUP_FILENAME to gs://$BUCKET_NAME/$FOLDER_NAME/"
-/root/gsutil/gsutil cp "$BACKUP_PATH""$BACKUP_FILENAME" gs://"$BUCKET_NAME"/"$FOLDER_NAME"/ 2>&1
-echo "Copying finished"
+echo "Copying $BACKUP_PATH$BACKUP_FILENAME to gs://$BUCKET_NAME/$DB_NAME/"
+/root/gsutil/gsutil cp "$BACKUP_PATH""$BACKUP_FILENAME" gs://"$BUCKET_NAME"/"$DB_NAME"/ 2>&1
+echo "Copying $DB_NAME finished"
 echo "Removing backup data"
 rm -rf $BACKUP_PATH*
+
+done
+
+echo "Copying finished"
